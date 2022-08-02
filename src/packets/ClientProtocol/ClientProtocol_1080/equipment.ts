669c5bec7acb51c035a0fbfa8b8d372b3406cd79
Ultrax1995 <110496808+Ultrax1995@users.noreply.github.com>
2022-08-03 00:10:52 +0200

    Add files via upload

---
+++ src/packets/ClientProtocol/ClientProtocol_1080/equipment.ts
@@ -0,0 +1,126 @@
+// ======================================================================
+//
+//   GNU GENERAL PUBLIC LICENSE
+//   Version 3, 29 June 2007
+//   copyright (C) 2020 - 2021 Quentin Gruber
+//   copyright (C) 2021 - 2022 H1emu community
+//
+//   https://github.com/QuentinGruber/h1z1-server
+//   https://www.npmjs.com/package/h1z1-server
+//
+//   Based on https://github.com/psemu/soe-network
+// ======================================================================
+
+import {
+  attachmentSchema,
+  equipmentCharacterSchema,
+  equipmentSlotSchema,
+} from "./shared";
+
+export const equipmentPackets: any = [
+  [
+    "Equipment.SetCharacterEquipment",
+    0x9501,
+    {
+      fields: [
+        {
+          name: "characterData",
+          type: "schema",
+          fields: equipmentCharacterSchema,
+        },
+        { name: "unknownDword1", type: "uint32", defaultValue: 0 },
+        { name: "unknownString1", type: "string", defaultValue: "" },
+        { name: "unknownString2", type: "string", defaultValue: "#" },
+        {
+          name: "equipmentSlots",
+          type: "array",
+          defaultValue: [],
+          fields: equipmentSlotSchema,
+        },
+        {
+          name: "attachmentData",
+          type: "array",
+          defaultValue: [],
+          fields: attachmentSchema,
+        },
+        { name: "unknownBoolean1", type: "boolean", defaultValue: false },
+      ],
+    },
+  ],
+  [
+    "Equipment.SetCharacterEquipmentSlot",
+    0x9502,
+    {
+      fields: [
+        {
+          name: "characterData",
+          type: "schema",
+          fields: equipmentCharacterSchema,
+        },
+        {
+          name: "equipmentSlot",
+          type: "schema",
+          fields: equipmentSlotSchema,
+        },
+        {
+          name: "attachmentData",
+          type: "schema",
+          fields: attachmentSchema,
+        },
+      ],
+    },
+  ],
+  [
+    "Equipment.UnsetCharacterEquipmentSlot",
+    0x9503,
+    {
+      fields: [
+        {
+          name: "characterData",
+          type: "schema",
+          fields: equipmentCharacterSchema,
+        },
+        { name: "unknownDword1", type: "uint32", defaultValue: 0 },
+        { name: "slotId", type: "uint32", defaultValue: 0 },
+      ],
+    },
+  ],
+  [
+    "Equipment.SetCharacterEquipmentSlots",
+    0x9504,
+    {
+      fields: [
+        {
+          name: "characterData",
+          type: "schema",
+          fields: equipmentCharacterSchema,
+        },
+        { name: "gameTime", type: "uint32", defaultValue: 0 },
+        {
+          name: "slots",
+          type: "array",
+          defaultValue: [],
+          fields: [
+            { name: "index", type: "uint32", defaultValue: 0 },
+            { name: "slotId", type: "uint32", defaultValue: 0 },
+          ],
+        },
+        { name: "unknownDword1", type: "uint32", defaultValue: 1 },
+        { name: "unknownString1", type: "string", defaultValue: "" },
+        { name: "unknownString2", type: "string", defaultValue: "#" },
+        {
+          name: "equipmentSlots",
+          type: "array",
+          defaultValue: [],
+          fields: equipmentSlotSchema,
+        },
+        {
+          name: "attachmentData",
+          type: "array",
+          defaultValue: [],
+          fields: attachmentSchema,
+        },
+      ],
+    },
+  ],
+];
---
+++ src/packets/packettable.ts
@@ -0,0 +1,33 @@
+// ======================================================================
+//
+//   GNU GENERAL PUBLIC LICENSE
+//   Version 3, 29 June 2007
+//   copyright (C) 2020 - 2021 Quentin Gruber
+//   copyright (C) 2021 - 2022 H1emu community
+//
+//   https://github.com/QuentinGruber/h1z1-server
+//   https://www.npmjs.com/package/h1z1-server
+//
+//   Based on https://github.com/psemu/soe-network
+// ======================================================================
+
+export default function PacketTableBuild(packets: any[]): any[] {
+  const packetTypes: any = {};
+  const packetDescriptors: any = {};
+  for (let i = 0; i < packets.length; i++) {
+    const packet = packets[i],
+      name = packet[0],
+      type = packet[1] >>> 0,
+      packetDesc = packet[2];
+    packetTypes[name] = type;
+    packetDescriptors[type] = {
+      type: type,
+      name: name,
+      schema: packetDesc.fields,
+      fn: packetDesc.fn,
+      parse: packetDesc.parse,
+      pack: packetDesc.pack,
+    };
+  }
+  return [packetTypes, packetDescriptors];
+}
